e3a8c10854fa6be10a6e44bdfeb31cc0a7c68588
silentunion <reecemilliner@gmail.com>
2023-04-21 10:53:35 -0700

    Switched the index to returning HTML

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ app = fastapi.FastAPI()
 
 @app.get('/')
 def index():
-    return {
-        "message": "Hello world"
-    }
+    content = """
+    <h1>Hello FastAPI</h1>
+
+    <div>This is a terrible way of doing this</div>
+    """
+    response = fastapi.responses.HTMLResponse(content)
+    return response
 
 
 if __name__ == '__main__':
