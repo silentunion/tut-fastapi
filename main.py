fc9685238badd4c80f213d59dce7628ce72d0787
silentunion <reecemilliner@gmail.com>
2023-04-21 11:28:04 -0700

    Added fastapi_chameleon to main to work with the templates folder

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import fastapi
 import uvicorn
+import fastapi_chameleon
+from fastapi_chameleon import template
 
 app = fastapi.FastAPI()
 
+fastapi_chameleon.global_init('templates')
+
 
 @app.get('/')
-def index():
-    content = """
-    <h1>Hello FastAPI</h1>
-
-    <div>This is a terrible way of doing this</div>
-    """
-    response = fastapi.responses.HTMLResponse(content)
-    return response
+@template(template_file='index.html')
+def index(user: str = 'anon'):
+    return {
+        'user_name': user if user else "anon"
+    }
 
 
 if __name__ == '__main__':
