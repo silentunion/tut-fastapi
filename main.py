942e8430e8aa34d0f7ae70132841b7db56c3d434
silentunion <reecemilliner@gmail.com>
2023-04-21 10:41:45 -0700

    Initial main.py code - end of ch 3

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def index():
+    return {
+        "message": "Hello world"
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
